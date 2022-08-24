27450fee3dbe8e1d5e59ac2bbee3e5743af6e932
Philipp Theyssen <p.theyssen@gmail.com>
2022-08-24 12:44:56 +0200

    Solve day2 task 2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,14 +1,38 @@
 use std::fs;
+use std::iter::zip;
 use itertools::Itertools;
 
 fn main () {
+    task1()
+}
+
+fn task1() {
     let contents = fs::read_to_string("input/day2.txt")
         .expect("Failed reading input");
 
-    let v: Vec<&str> = contents.split('\n')
+    let v: Vec<&str> = contents.split_terminator('\n')
         .collect();
+    let pairs: Vec<Vec<&&str>> = v.iter().combinations(2).collect();
 
-    let it = (1..5).combinations(3);
-    // println!("v:\n{:?}", v);
-    println!("it:\n{:?}", it);
+    let result: Vec<i32> = pairs.iter()
+        .map(|p| compute_char_distance(p.to_vec()))
+        .collect();
+}
+
+fn compute_char_distance(p: Vec<&&str>) -> i32 {
+    let s1 = p[0];
+    let s2 = p[1];
+
+    let mut diffs = 0;
+    for (c1, c2) in zip(s1.chars(), s2.chars()) {
+        if c1 != c2 {
+            diffs += 1;
+        }
+    }
+    if diffs == 1 {
+        println!("solution: {}, {}", s1, s2);
+        // solution: cvgywxqubnuaefmsldjdrpfzyi, cvgywxqubnuaefmslkjdrpfzyi
+        // -> cvgywxqubnuaefmsljdrpfzyi
+    }
+    diffs
 }
